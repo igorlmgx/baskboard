48e850f37bf09e7899a31d54d11b7c2bfa635a78
igorlmgx <igorlmgx@gmail.com>
2020-10-12 15:50:18 -0300

    Add some responsivity to TeamCard

--- components/TeamCard.vue
+++ components/TeamCard.vue
@@ -1,50 +1,49 @@
 <template>
-  <v-hover>
-    <template v-slot="{ hover }">
-      <v-card
-        :color="'#' + team.primary_color"
-        :style="'border-left: 1rem solid #' + team.secondary_color"
-        :elevation="hover ? 5 : 2"
-        dark
-      >
-        <v-row no-gutters style="height: 110px" align="center">
-          <v-col cols="1">
-            <v-img
-              height="110px"
-              style="background-color: #fff"
-              contain
-              :src="team.wikipedia_logo_url"
-            ></v-img>
-          </v-col>
-          <v-col cols="2" class="text-center">
-            <h2>{{ team.city }}</h2>
-            <h2>{{ team.name }}</h2>
-          </v-col>
-          <v-col cols="2" class="text-center">
-            <h2>{{ team[attr1.value] }}</h2>
-            <h4>{{ attr1.text }}</h4>
-          </v-col>
-          <v-col cols="2" class="text-center">
-            <h2>{{ team[attr2.value] }}</h2>
-            <h4>{{ attr2.text }}</h4>
-          </v-col>
-          <v-col cols="2" class="text-center">
-            <h2>{{ team[attr3.value] }}</h2>
-            <h4>{{ attr3.text }}</h4>
-          </v-col>
-          <v-col cols="2" class="text-center">
-            <h2>{{ team[attr4.value] }}</h2>
-            <h4>{{ attr4.text }}</h4>
-          </v-col>
-          <v-col cols="1" class="text-center">
-            <v-btn :to="'/teams/' + team.team_id" nuxt fab light small>
-              <v-icon>mdi-arrow-right</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card>
-    </template>
-  </v-hover>
+  <v-row
+    no-gutters
+    :style="
+      'background-color: #' +
+      team.primary_color +
+      '; border-left: 1rem solid #' +
+      team.secondary_color
+    "
+    class="team-row"
+    align="center"
+  >
+    <v-col cols="12" md="1">
+      <v-img
+        height="110px"
+        style="background-color: #fff"
+        contain
+        :src="team.wikipedia_logo_url"
+      ></v-img>
+    </v-col>
+    <v-col cols="12" md="2" class="text-center">
+      <h2>{{ team.city }}</h2>
+      <h2>{{ team.name }}</h2>
+    </v-col>
+    <v-col cols="12" md="2" class="text-center">
+      <h2>{{ team[attr1.value] }}</h2>
+      <h4>{{ attr1.text }}</h4>
+    </v-col>
+    <v-col cols="12" md="2" class="text-center">
+      <h2>{{ team[attr2.value] }}</h2>
+      <h4>{{ attr2.text }}</h4>
+    </v-col>
+    <v-col cols="12" md="2" class="text-center">
+      <h2>{{ team[attr3.value] }}</h2>
+      <h4>{{ attr3.text }}</h4>
+    </v-col>
+    <v-col cols="12" md="2" class="text-center">
+      <h2>{{ team[attr4.value] }}</h2>
+      <h4>{{ attr4.text }}</h4>
+    </v-col>
+    <v-col cols="12" md="1" class="btn-details">
+      <v-btn :to="'/teams/' + team.team_id" nuxt fab light small>
+        <v-icon>mdi-arrow-right</v-icon>
+      </v-btn>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -58,3 +57,20 @@ export default {
   },
 };
 </script>
+
+<style>
+.team-row {
+  border-radius: 0.5rem;
+}
+.btn-details {
+  text-align: center;
+}
+@media (max-width: 959px) {
+  .btn-details {
+    text-align: right;
+  }
+  .team-row .v-btn {
+    margin: 0 0.5rem 0.5rem 0;
+  }
+}
+</style>
